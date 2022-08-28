398ed73bc35c8beb16aa6dee30724fbf91b45860
Ektora <zsoltlucski@gmail.com>
2022-08-28 15:28:10 +0200

    Két mátrix összeadása

--- Gyakorlo3/Program.cs
+++ Gyakorlo3/Program.cs
@@ -4,7 +4,8 @@ class Gyakorlo3
 
     public static void Main(string[] args)
     {
-
+        // 4-es minta keresése két dimenziós tömbben
+        /*
         int[,] tomb = new int[,] {
             {4,1,4,2,5},
             {4,6,3,2,1},
@@ -21,7 +22,19 @@ class Gyakorlo3
         }
 
         Console.WriteLine("Van 4-es minta a kétdimenziós tömbben: " + isConsecutiveFour(tomb));
+        */
 
+        double[,] a = { {2,4,7}, {1,5,3} };
+        double[,] b = { { 1, 7, -2 }, { 4, 2, 1 } };
+        double[,] result = addMatrix(a, b);
+        if(result != null)
+        {
+            tombKiIras(result);
+        }
+        else
+        {
+            Console.WriteLine("A két mátrix nem adható össze");
+        }
     }
 
     public static bool isConsecutiveFour(int[,] values)
@@ -39,6 +52,35 @@ class Gyakorlo3
             }
         }
         return false;
+    }
+
+    public static double[,] addMatrix(double[,] a, double[,] b)
+    {
+        if(a.GetLength(0) == b.GetLength(0) && a.GetLength(1) == b.GetLength(1))
+        {
+            double[,] result = new double[a.GetLength(0), a.GetLength(1)];
+            for(int i = 0; i < a.GetLength(0); i++)
+            {
+                for(int j= 0; j < a.GetLength(1); j++)
+                {
+                    result[i, j] = a[i, j] + b[i, j];
+                }
+            }
+            return result;
+        }
+        return null;
+    }
+
+    public static void tombKiIras(double[,] tomb)
+    {
+        for (int i = 0; i < tomb.GetLength(0); i++)
+        {
+            for (int j = 0; j < tomb.GetLength(1); j++)
+            {
+                Console.Write(tomb[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
     }
 
 }
